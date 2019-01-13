d0ecfded0eb28b8bc21ab979879169adb14bfc28
George Damion lannister <gdamion-@e3r1p1.21-school.ru>
2019-01-13 11:57:25 +0300

    now it's time to fight against leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,109 +6,57 @@
 /*   By: gdamion- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/16 15:19:17 by gdamion-          #+#    #+#             */
-/*   Updated: 2019/01/11 14:04:10 by gdamion-         ###   ########.fr       */
+/*   Updated: 2019/01/13 11:51:08 by gdamion-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//read, malloc, free - allowed!
-
 #include "get_next_line.h"
 
-/*
-// pick 1-st line. WORKING!!!
-int		get_one_line(char **line, const int fd, char *content, size_t *endl)
+static int	get_one_line(char **line, const int fd,
+						char **content, size_t *endl)
 {
 	char	buf[BUFF_SIZE + 1];
-	int	rd;
-	int i;
-	static t_list files[FD_STORE_SIZE];
+	int		rd;
+	size_t	i;
 
-	if (fd < 0 || !line) //проверяем входящие значения на ошибку или отсутствие
+	ft_bzero(buf, BUFF_SIZE + 1);
+	rd = read(fd, buf, BUFF_SIZE);
+	if (rd == -1 || !(*content = ft_strjoin(*content, buf)))
 		return (-1);
-
-	ALLOC_MEM(*line, char*, 1);
-	**line = '\0';
-
-	if (!content)
+	i = *endl + 1;
+	while ((*content)[i] != '\0')
 	{
-		ALLOC_MEM(content, char*, 1);
-		*content = '\0';
-		*endl = 0;
-	}
-	ft_bzero(buf, BUFF_SIZE + 1); //заполняем буфер терминаторами
-	rd = read(fd, buf, BUFF_SIZE); //читаем
-	printf("\nread: %d\n", rd);
-	printf("buf:\n%s\n\n", buf);
-	if (rd == -1 || !(content = ft_strjoin(content, buf))) //ошибка чтения и ошибка объединения
-		return (-1);
-	printf("content:\n%s\n\n", content);
-	i = *endl;
-	while (content[i] != '\0')
-	{
-		if (content[i] == '\n')
-			break;
+		if ((*content)[i] == '\n')
+			break ;
 		i++;
 	}
-	//strsub here!
-	*line = ft_strsub(content, *endl, i - *endl);
-	printf("line:\n%s\n\n", *line);
-	if (content[i] == '\n')
+	*line = ft_strsub(*content, *endl + 1, i - *endl - 1);
+	if (rd == 0 && i == *endl + 1 && (*content)[i] == '\0')
+		return (0);
+	else if ((*content)[i] == '\n' ||
+			((*content)[i] == '\0' && (rd == 0 || rd < BUFF_SIZE)))
 	{
 		*endl = i;
 		return (1);
 	}
-	if (rd == 0 || rd < BUFF_SIZE)
-		return (0);
-	else
-		return (get_one_line(line, fd, content, endl));
+	return (get_one_line(line, fd, content, endl));
 }
-*/
 
-int		get_one_line(char **line, const int fd, char **content, size_t *endl)
+int			get_next_line(const int fd, char **line)
 {
-	char	buf[BUFF_SIZE + 1];
-	int	rd;
-	int i;
+	static t_files	files[FD_STORE_SIZE];
+	int				res;
 
-	ft_bzero(buf, BUFF_SIZE + 1); //заполняем буфер терминаторами
-	rd = read(fd, buf, BUFF_SIZE); //читаем
-	if (rd == -1 || !(*content = ft_strjoin(*content, buf))) //ошибка чтения и ошибка объединения
+	if (fd < 0 || !line || BUFF_SIZE < 1)
 		return (-1);
-	i = *endl;
-	while ((*content)[i] != '\0')
-	{
-		if ((*content)[i] == '\n')
-			break;
-		i++;
-	}
-	*line = ft_strsub(*content, *endl, i - *endl);
-	if ((*content)[i] == '\n')
-	{
-		*endl = i + 1;
-		return (1);
-	}
-	if (rd == 0 || rd < BUFF_SIZE)
-		return (0);
-	else
-		return (get_one_line(line, fd, content, endl));
-}
-//25 strings !!!!!!
-
-int		get_next_line(const int fd, char **line)
-{
-	static t_files files[FD_STORE_SIZE];
-	int res;
-
-	if (fd < 0 || !line) //проверяем входящие значения на ошибку или отсутствие
+	if (!(*line = ft_strnew(0)))
 		return (-1);
-	ALLOC_MEM(*line, char*, 1);
-	**line = '\0';
 	if (!(files[fd].content))
 	{
-		ALLOC_MEM(files[fd].content, char*, 1);
-		*files[fd].content = '\0';
-		files[fd].content_size = 0;
+		if (!(files[fd].content = ft_strnew(0)))
+			return (-1);
+		files[fd].endl = -1;
 	}
-	res = get_one_line(line, fd, &files[fd].content, &files[fd].content_size);
+	res = get_one_line(line, fd, &files[fd].content, &files[fd].endl);
 	return (res);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,30 +6,24 @@
 /*   By: gdamion- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/16 15:20:04 by gdamion-          #+#    #+#             */
-/*   Updated: 2019/01/11 14:00:29 by gdamion-         ###   ########.fr       */
+/*   Updated: 2019/01/13 11:50:56 by gdamion-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 200
-# define FD_STORE_SIZE 200
-# define ALLOC_MEM(x, y, z)\
-			x=(y)malloc(z);\
-			if (!x)\
-				return (-1)
-
 # include "libft/libft.h"
+
+# define BUFF_SIZE 200
+# define FD_STORE_SIZE 5000
 
 typedef struct		s_files
 {
 	char			*content;
-	size_t			content_size;
+	size_t			endl;
 }					t_files;
 
-int		get_next_line(const int fd, char **line);
-int		get_one_line(char **line, const int fd, char **content, size_t *endl);
-//char	*ft_strjoin_mod(char *s1, char  *s2, int *n);
+int					get_next_line(const int fd, char **line);
 
 #endif
