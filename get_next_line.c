c08bf0d01828ec8dacce27dc2a42b85488ec64ea
slaveareyou <45574608+slaveareyou@users.noreply.github.com>
2019-01-14 21:02:06 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -44,11 +44,11 @@ static int	get_one_line(char **line, const int fd, char **c, int *e)
 	if (((*e == -1 && (*c)[i] == '\0')) || (*e != -1 && (*c)[*e] == '\0'))
 		return (0);
 	gol_ops_two(&i, c);
+	if (rd == 0 && (i == *e + 1) && (*c)[i] == '\0')
+		return (0);
 	if (!(tmp = ft_strsub(*c, *e + 1, i - *e - 1)))
 		return (-1);
 	gol_ops_one(line, &tmp);
-	if (rd == 0 && (i == *e + 1) && (*c)[i] == '\0')
-		return (0);
 	if ((*c)[i] == '\n' || ((*c)[i] == '\0' && (rd == 0 || rd < BUFF_SIZE)))
 	{
 		*e = i;
@@ -62,8 +62,6 @@ int			get_next_line(const int fd, char **line)
 	static t_files	files[FD_STORE_SIZE];
 
 	if (fd < 0 || !line || BUFF_SIZE < 1)
-		return (-1);
-	if (!(*line = ft_strnew(0)))
 		return (-1);
 	if (!(files[fd].content))
 	{
